0109bb44edf024d65d1bf9e89012a08452c9863b
cian74 <cianbarrett3011@gmail.com>
2025-04-04 18:54:10 +0100

    setup initial home page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomePage } from "./home/home.page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomePage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
