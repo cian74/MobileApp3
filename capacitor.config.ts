83b248a00d65946f458c5c0c06352c17dba6ea99
cian74 <cianbarrett3011@gmail.com>
2025-04-09 10:39:47 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mobile-app-3rdyr',
+  webDir: 'www'
+};
+
+export default config;
